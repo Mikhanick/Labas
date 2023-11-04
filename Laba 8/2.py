8cb5a51b28c85f1359f2598a067fc20ae4f540d4
Mikhanick <mihailsavatey@gmail.com>
2023-11-04 11:03:53 +0000

    Лаба 8 готова не защищена

--- Laba 8/2.py
+++ Laba 8/2.py
@@ -1,44 +1,49 @@
-matrix = []
+# Саватеев Михаил группа ИУ7-16Б
+# Лабораторная работа номер 8, задание 22
+matrix = [] #инициализируем матрицу
 print('Для ввода матрицы напишите через пробел значения строк, затем нажмите Enter. По окончании ввода строк нажмите '
       'Enter')
 while True:
-    line = [int(i) for i in input().split()]
+    line = [int(i) for i in input().split()] # ввод строки в матрицу
     if matrix == []:
-        matrix.append(line)
-    elif line == []:
+        matrix.append(line) # Если матрица еще пустая то просто добавляем любую строку
+    elif line == []: # если введена пустая строка то завершаем ввод матрицы
         print('Ввод матрицы завершен.')
         break
-    elif len(matrix[0]) == len (line):
+    elif len(matrix[0]) == len (line): # если длина строки равна длине первой строке, иначе выводим ошибку и просим
+        # повторить ввод
         matrix.append(line)
     else:
         print(f'Ошибка. Длина строк в матрице должна совпадать, и должна быть равна {len(matrix[0])}.\nПовторите ввод!')
 if len(matrix) == 0:
-    print("Ошибка. Вы ввели пустую матрицу. Работа программы завершена с ошибкой!")
+    print("Ошибка. Вы ввели пустую матрицу. Работа программы завершена с ошибкой!") # выводим ошибку что не совпадает
+    # количестао элементов в строке с отальной матрицей
 else:
-
-    max_cnt = float('-inf')
-    min_cnt = float('inf')
-
+    # инициализируем макс/мин элементы и индексы на котороых они находятся
+    max_cnt = 0
+    min_cnt = len(matrix[0])
     index_max = -1
     index_min = -1
 
-    for i in range(len(matrix)):
+    for i in range(len(matrix)): # перебираем строки в матрицце
         cnt = 0
-        for j in matrix[i]:
+        for j in matrix[i]: # если отрицательное прибавлем счетчик
             if j < 0:
                 cnt += 1
         #print('cnt =', cnt,'max count', max_cnt,'min_count', min_cnt)
-        if cnt>max_cnt:
+        if cnt>max_cnt: # если счетчик больше максимального
             max_cnt = cnt
             index_max = i
 
-        if cnt < min_cnt and cnt > 0:
+        if cnt < min_cnt and cnt > 0: # если счетчик меньше миниума но больше нуля
             min_cnt = cnt
             index_min = i
 
     #print(index_max,index_min)
-    if index_min > -1 and index_max > -1:
+    if index_min > -1 and index_max!=index_min: #если встретились и максимальное и минимальное количество то меняем их строки
         matrix[index_max], matrix[index_min] = matrix[index_min],matrix[index_max]
 
-    for line in matrix:
-        print(*line)
+    for line in matrix:  # вывод
+        for el in line:
+            print(f'{el:5.5g}', end='')
+        print()
--- Laba 8/4.py
+++ Laba 8/4.py
@@ -1,41 +1,48 @@
-matrix = []
+# Саватеев Михаил группа ИУ7-16Б
+# Лабораторная работа номер 8, задание 4
+matrix = [] #инициализируем матрицу
 print('Для ввода матрицы напишите через пробел значения строк, затем нажмите Enter. По окончании ввода строк нажмите '
       'Enter')
 while True:
-    line = [int(i) for i in input().split()]
+    line = [int(i) for i in input().split()] # ввод строки в матрицу
     if matrix == []:
-        matrix.append(line)
-    elif line == []:
+        matrix.append(line) # Если матрица еще пустая то просто добавляем любую строку
+    elif line == []: # если введена пустая строка то завершаем ввод матрицы
         print('Ввод матрицы завершен.')
         break
-    elif len(matrix[0]) == len (line):
+    elif len(matrix[0]) == len (line): # если длина строки равна длине первой строке, иначе выводим ошибку и просим
+        # повторить ввод
         matrix.append(line)
     else:
         print(f'Ошибка. Длина строк в матрице должна совпадать, и должна быть равна {len(matrix[0])}.\nПовторите ввод!')
 if len(matrix) == 0:
-    print("Ошибка. Вы ввели пустую матрицу. Работа программы завершена с ошибкой!")
+    print("Ошибка. Вы ввели пустую матрицу. Работа программы завершена с ошибкой!") # выводим ошибку что не совпадает
+    # количестао элементов в строке с отальной матрицей
 else:
+    # инициализируем переменные
     min_index = -1
     max_index = -1
-    min_sum = float('inf')
-    max_sum = float('-inf')
-    for i in range(len(matrix[0])):
+    min_sum = 0
+    max_sum = 0
+    for i in range(len(matrix[0])): # перебираем индексы элементов в строках
         sm = 0
-        for j in range(len(matrix)):
+        for j in range(len(matrix)): # перебираем столбцы и считаем сумму в них
             sm += matrix[j][i]
-        if 0 < sm < min_sum:
+        if sm < min_sum or min_index == -1: # если сумма минимальна то запоминаем индекс столбца
             # print('min new')
             min_sum = sm
             min_index = i
-        if sm>max_sum:
+        if sm>max_sum or max_index == -1: # если сумма максимально то запоминаем индекс столбца
             # print('max new')
             max_sum = sm
             max_index = i
         # print(sm,max_sum, min_sum)
     # print(max_index,min_index)
 
-    for i in range(len(matrix)):
+    for i in range(len(matrix)): # меняем лбцы местами
         matrix[i][max_index],matrix[i][min_index] = matrix[i][min_index],matrix[i][max_index]
 
-    for line in matrix:
-        print(*line)
+    for line in matrix: # вывод
+        for el in line:
+            print(f'{el:5.5g}',end='')
+        print()
