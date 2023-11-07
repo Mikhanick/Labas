294756c7f2b0b8e9b8eb6172735e6ff95638d263
Mikhanick <103630871+Mikhanick@users.noreply.github.com>
2023-11-07 11:25:20 +0300

    исправил номер 1 в Лабе 9

--- Laba 9/1.py
+++ Laba 9/1.py
@@ -23,16 +23,25 @@ L = []
 for j in range(len(arrayD)):
     AV.append(0)
     L.append(0)
+    cnt=0
     for k in range(len(arrayF)):
-        AV[-1]+=matrixA[j][k]
-    AV[-1]/=(k+1)
-    for k in range(len(arrayF)):
-        if matrixA[j][k]<AV[-1]:
-            L[-1]+=1
-
+        if matrixA[j][k]>0:
+            cnt +=1
+            AV[-1]+=matrixA[j][k]
+    if cnt>0:
+        AV[-1]/=cnt
+        for k in range(len(arrayF)):
+            if matrixA[j][k]<AV[-1]:
+                L[-1]+=1
+    else:
+        L[-1] = None
+        AV[-1] = None
 
 
 for line in range(len(matrixA)):  # вывод
     for el in matrixA[line]:
         print(f'{el:11.6g}', end='')
-    print(f' | {AV[line]:11.6g} {L[line]:<11.6g}')
+    if AV[line]!=None:
+        print(f' | {AV[line]:11.6g} {L[line]:<11.6g}')
+    else:
+        print(' |        None None')
