5618660aa5ac7ad8ab04dd17c137febb8c4326b0
Mikhanick <103630871+Mikhanick@users.noreply.github.com>
2023-11-19 22:03:19 +0300

    Сделана лаба 10

---
+++ Laba 10/user_inputs.py
@@ -0,0 +1,8 @@
+NEEDED_ACCURACY = 0.0001 # необходимая точность
+
+def function (x): # функция
+    return x**2 + 2*x + 1
+
+def integral (x): # интеграл функции
+    y = x**3/3 + x**2 + x
+    return y
